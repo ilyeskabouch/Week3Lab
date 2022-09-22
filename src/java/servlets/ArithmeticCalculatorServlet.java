6ced8a7f1db04db7cca281e24d9878604a648c2e
ilyes <ilyes@MSI>
2022-09-22 13:14:24 -0600

    Bug Fixes. Fixed a bug with ArithmeticCalculatorServlet that was making the doPost method return the wrong JSP file. Also fixed and error in the arithmeticCalculator where it was trying to compare two string using == instead of the .equals method.

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -33,16 +33,16 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
         try {
             int int_first = Integer.parseInt(first);
             int int_second = Integer.parseInt(second);
-            if (operation == "+") {
+            if (operation.equals("+")) {
                 request.setAttribute("msg", int_first + int_second);
             }
-            else if (operation == "-") {
+            else if (operation.equals("-")) {
                 request.setAttribute("msg", int_first - int_second);
             }
-            else if (operation == "*") {
+            else if (operation.equals("*")) {
                 request.setAttribute("msg", int_first * int_second);
             }
-            else if (operation == "%") {
+            else if (operation.equals("%")) {
                 request.setAttribute("msg", int_first / int_second);
             }
             else {
@@ -51,7 +51,7 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
         } catch (Exception e) {
             request.setAttribute("msg", "Invalid");
         }
-        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticCalculator.jsp")
                 .forward(request, response);
     }
 }
